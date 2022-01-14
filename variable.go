cbeb00a6cea422ed36132965baf8b1f36380e6a1
desrilfatra <desrilfatra8@gmail.com>
2022-01-14 20:35:39 +0700

    variable

---
+++ variable.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	name = "Desril Fatra"
+	fmt.Println(name)
+
+	name = "Desril Aditama"
+	fmt.Println(name)
+
+	var friendName = "Rafif"
+	fmt.Println(friendName)
+
+	var age = 20
+	fmt.Println(age)
+
+	country := "Indonesia"
+	fmt.Println(country)
+
+	var(
+		firstName = "Desril"
+		lastName = "Fatra"
+	) 
+	fmt.Println(firstName, lastName)
+}
