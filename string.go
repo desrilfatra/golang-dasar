dd0050b7ad2c4cec1d9e01afadb1dee9cff4ea3e
desrilfatra <desrilfatra8@gmail.com>
2022-01-14 20:35:29 +0700

    string

---
+++ string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Desril")
+	fmt.Println("Desril Fatra")
+	fmt.Println("Desril Fatra Aditama")
+
+	fmt.Println(len("Desril"))
+	fmt.Println("Desril Fatra"[0])
+	fmt.Println("Desril Fatra Aditama"[1])
+}
