5f32481673bdf6441b39796f6cd955c901246bde
desrilfatra <desrilfatra8@gmail.com>
2022-02-26 14:33:00 +0700

    panic

---
+++ panic.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func endApp() {
+	message := recover()
+	if message != nil {
+		fmt.Println("Error dengan message:", message)
+	}
+	fmt.Println("Aplikasi Selesai")
+}
+
+func runApp(error bool) {
+	defer endApp()
+	if error {
+		panic("Terjadi Kesalahan")
+	}
+
+	fmt.Println("Aplikasi berjalan")
+}
+
+func main() {
+	runApp(true)
+	fmt.Println("Desril")
+}
