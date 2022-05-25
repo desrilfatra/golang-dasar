3a54ff1922da5a370645fcc4e278aee8fdf058bd
Desril Fatra <desrilfatra8@gmail.com>
2022-05-25 07:15:49 +0700

    struct method

--- struct.go
+++ struct.go
@@ -3,18 +3,36 @@ package main
 import "fmt"
 
 type Customer struct {
-	Name    string
-	Address string
-	Age     int
+	Name, Address string
+	Age           int
+}
+
+func (customer Customer) sayHi(name string) {
+	fmt.Println("Hello", name, "My name is", customer.Name)
+}
+
+func (a Customer) sayHai() {
+	fmt.Println("Hello a", a.Name)
 }
 
 func main() {
-	var Desril Customer
-	Desril.Name = "Desril"
-	Desril.Address = "Tangsel"
-	Desril.Age = 23
+	var eko Customer
+	eko.Name = "Eko"
+	eko.Address = "Jakarta"
+	eko.Age = 25
 
-	fmt.Println(Desril.Name)
-	fmt.Println(Desril.Address)
-	fmt.Println(Desril.Age)
+	eko.sayHi("Joko")
+	eko.sayHai()
+
+	// fmt.Println(eko)
+
+	// joko := Customer{
+	// 	Name:    "Joko",
+	// 	Address: "Bandung",
+	// 	Age:     25,
+	// }
+	// fmt.Println(joko)
+
+	// budi := Customer{"Budi", "Jakarta", 25}
+	// fmt.Println(budi)
 }
